00cfd29db066709cf456e12a8ed7de4eb98863d7
Stephan Cilliers <stephanus.cilliers@gmail.com>
2021-07-18 22:15:08 +0200

    Only handle applicable commands

--- bot.js
+++ bot.js
@@ -22,6 +22,12 @@ const bot = new TelegramBot(token, botOptions)
 
 bot.setWebHook(`${url}/bot${token}`)
 
+function deleteProcessingMessage(chatId, messagePromise) {
+  messagePromise.then(m => {
+    bot.deleteMessage(chatId, m.message_id)
+  })
+}
+
 const genericCommandMatcher = /(?<prefix>(?:^|^[\s\n\t\r]+)\/(?:[\s\n\t\r]*)|(?<=[\s\n\t\r])\.:(?:[\s\n\t\r]*)|(?<=[\s\n\t\r])::(?:[\s\n\t\r]*))(?<command>[a-zA-Z0-9]+)(?:[\s\n\t\r]*)(?<arguments>[^]*?(?=(?<=[\s\n\t\r])\.:|(?<=[\s\n\t\r])::|$))/
 bot.onText(genericCommandMatcher, (msg, match) => {
   console.log(match.groups.command)
@@ -30,26 +36,23 @@ bot.onText(genericCommandMatcher, (msg, match) => {
   const params = match.groups.arguments.split(" ")
   const chatId = msg.chat.id
 
-  try {
-    util[command](...params).then(r => {
-      const wrapped = {result: r}
-
-      console.log(JSON.stringify(wrapped.result, null, "\t"))
-
-      bot.sendMessage(chatId, JSON.stringify(wrapped.result, null, "\t"))
-    }).catch(() => {
-      bot.sendMessage(chatId, "Unknown command")
-    })
-  } catch (error) {
-    bot.sendMessage(chatId, "Unknown command")
+  if (!(command in util)) {
+    return
   }
-})
 
-// Listen for any kind of message. There are different kinds of
-// messages.
-bot.on("message", (msg) => {
-  const chatId = msg.chat.id
+  const processingPromise = bot.sendMessage(chatId, "Processing...")
 
-  // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, "Processing...")
+  util[command](...params).then(r => {
+    const wrapped = {result: r}
+
+    console.log(JSON.stringify(wrapped.result, null, "\t"))
+
+    bot.sendMessage(chatId, JSON.stringify(wrapped.result, null, "\t")).then(() => {
+      deleteProcessingMessage(chatId, processingPromise)
+    })
+  }).catch(() => {
+    bot.sendMessage(chatId, "Unknown command").then(() => {
+      deleteProcessingMessage(chatId, processingPromise)
+    })
+  })
 })
